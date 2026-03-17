5bb56f96a3d2087f285ba77445865d4a5eb4a506
Alex Warnes <warnes.alexander@gmail.com>
2026-03-16 17:19:06 -0700

    Update Queries object

--- src/shopify/queries/__init__.py
+++ src/shopify/queries/__init__.py
@@ -2,11 +2,20 @@
 
 import os
 from pathlib import Path
+from dataclasses import dataclass
 
+@dataclass
 class Queries:
     """Class for managing GraphQL query files"""
+
     _query_directory = os.path.dirname(os.path.abspath(__file__))
 
-    check_file_status = Path(os.path.join(_query_directory, 'check_file_status.graphql')).read_text(encoding="utf-8")
-    current_app_installation = Path(os.path.join(_query_directory, 'current_app_installation.graphql')).read_text(encoding="utf-8")
-    get_products = Path(os.path.join(_query_directory, 'get_products.graphql')).read_text(encoding="utf-8")
+    check_file_status = Path(
+        os.path.join(_query_directory, "check_file_status.graphql")
+    ).read_text(encoding="utf-8")
+    current_app_installation = Path(
+        os.path.join(_query_directory, "current_app_installation.graphql")
+    ).read_text(encoding="utf-8")
+    get_products = Path(
+        os.path.join(_query_directory, "get_products.graphql")
+    ).read_text(encoding="utf-8")

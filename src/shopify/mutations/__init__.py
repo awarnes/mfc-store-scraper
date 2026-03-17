df72d26f0992588e59c5e130b471dd4679c10d93
Alex Warnes <warnes.alexander@gmail.com>
2026-03-16 17:19:18 -0700

    Add mutations and update Mutations object

--- src/shopify/mutations/__init__.py
+++ src/shopify/mutations/__init__.py
@@ -2,13 +2,38 @@
 
 import os
 from pathlib import Path
+from dataclasses import dataclass
 
+@dataclass
 class Mutations:
     """Class for managing GraphQL mutation files"""
+
     _query_directory = os.path.dirname(os.path.abspath(__file__))
 
-    file_create = Path(os.path.join(_query_directory, 'file_create.graphql')).read_text(encoding="utf-8")
-    generate_staged_uploads = Path(os.path.join(_query_directory, 'generate_staged_uploads.graphql')).read_text(encoding="utf-8")
-    product_create = Path(os.path.join(_query_directory, 'product_create.graphql')).read_text(encoding="utf-8")
-    product_update = Path(os.path.join(_query_directory, 'product_update.graphql')).read_text(encoding="utf-8")
-    product_variants_bulk_update = Path(os.path.join(_query_directory, 'product_variants_bulk_update.graphql')).read_text(encoding="utf-8")
+    file_create = Path(os.path.join(_query_directory, "file_create.graphql")).read_text(
+        encoding="utf-8"
+    )
+
+    generate_staged_uploads = Path(
+        os.path.join(_query_directory, "generate_staged_uploads.graphql")
+    ).read_text(encoding="utf-8")
+
+    product_create = Path(
+        os.path.join(_query_directory, "product_create.graphql")
+    ).read_text(encoding="utf-8")
+
+    product_update = Path(
+        os.path.join(_query_directory, "product_update.graphql")
+    ).read_text(encoding="utf-8")
+
+    product_variants_bulk_create = Path(
+        os.path.join(_query_directory, "product_variants_bulk_create.graphql")
+    ).read_text(encoding="utf-8")
+
+    product_variants_bulk_update = Path(
+        os.path.join(_query_directory, "product_variants_bulk_update.graphql")
+    ).read_text(encoding="utf-8")
+
+    product_option_update = Path(
+        os.path.join(_query_directory, "product_option_update.graphql")
+    ).read_text(encoding="utf-8")
